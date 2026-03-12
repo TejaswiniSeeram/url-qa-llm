02ff1c8434b42e1e68064f826bb465aa3266b9cf
Tejaswini Seeram <tejaswiniseeram@gmail.com>
2026-03-12 12:48:03 -0500

    ...

---
+++ rag.py
@@ -0,0 +1,108 @@
+# @Author: Dhaval Patel Copyrights Codebasics Inc. and LearnerX Pvt Ltd.
+
+from uuid import uuid4
+from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
+from pathlib import Path
+from langchain_community.document_loaders import WebBaseLoader # pyright: ignore[reportMissingImports]
+from langchain_text_splitters import RecursiveCharacterTextSplitter # pyright: ignore[reportMissingImports]
+from langchain_chroma import Chroma # pyright: ignore[reportMissingImports]
+from langchain_groq import ChatGroq # pyright: ignore[reportMissingImports]
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings # pyright: ignore[reportMissingImports]
+
+load_dotenv()
+import os
+os.environ["USER_AGENT"] = "Mozilla/5.0"
+# Constants
+CHUNK_SIZE = 1000
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+VECTORSTORE_DIR = Path(__file__).parent / "resources/vectorstore"
+COLLECTION_NAME = "real_estate"
+
+llm = None
+vector_store = None
+
+
+def initialize_components():
+    global llm, vector_store
+
+    if llm is None:
+        llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.9, max_tokens=500)
+
+    if vector_store is None:
+        ef = HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL,
+            model_kwargs={"trust_remote_code": True}
+        )
+
+        vector_store = Chroma(
+            collection_name=COLLECTION_NAME,
+            embedding_function=ef,
+            persist_directory=str(VECTORSTORE_DIR)
+        )
+
+
+def process_urls(urls):
+    """
+    This function scraps data from a url and stores it in a vector db
+    :param urls: input urls
+    :return:
+    """
+    yield "Initializing Components"
+    initialize_components()
+
+    yield "Resetting vector store...✅"
+    vector_store.reset_collection()
+
+    yield "Loading data...✅"
+    loader = WebBaseLoader(urls)
+    data = loader.load()
+
+    yield "Splitting text into chunks...✅"
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ".", " "],
+        chunk_size=CHUNK_SIZE
+    )
+    docs = text_splitter.split_documents(data)
+
+    yield "Add chunks to vector database...✅"
+    uuids = [str(uuid4()) for _ in range(len(docs))]
+    vector_store.add_documents(docs, ids=uuids)
+
+    yield "Done adding docs to vector database...✅"
+
+def generate_answer(query):
+    if not vector_store:
+        raise RuntimeError("Vector database is not initialized")
+
+    retriever = vector_store.as_retriever()
+
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+        Answer the question using the context below.
+
+        Context:
+        {context}
+
+        Question: {query}
+        """
+
+    response = llm.invoke(prompt)
+
+    sources = "\n".join([doc.metadata.get("source", "") for doc in docs])
+
+    return response.content, sources
+
+
+if __name__ == "__main__":
+    urls = [
+        "https://www.cnbc.com/2024/12/21/how-the-federal-reserves-rate-policy-affects-mortgages.html",
+        "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html"
+    ]
+
+    process_urls(urls)
+    answer, sources = generate_answer("Tell me what was the 30 year fixed mortagate rate along with the date?")
+    print(f"Answer: {answer}")
+    print(f"Sources: {sources}")
